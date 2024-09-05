221681129666cef258f4c5a334a0d15b3fd1b783
Alexei Mereniuc <alexei.mereniuc@isa.utm.md>
2024-09-05 10:57:14 +0300

    Search

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
+import { CarProps } from "@/types";
 
 
 export async function fetchCars() {
@@ -8,7 +9,7 @@ export async function fetchCars() {
     const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {headers: headers});
     const result = await response.json();
     return result;
-}
+};
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
     const basePricePerDay = 50; // Base rental price per day in dollars
@@ -23,4 +24,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
   
     return rentalRatePerDay.toFixed(0);
-  };
+};
+
+export const generateCarIgameUrl = (car: CarProps, angle?: string) => {
+    const url = new URL('https://cdn.imagin./studio/getimage');
+    const { make, year, model } = car;
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(' ')[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    url.searchParams.append('angle', `${angle}`);
+    
+    return `${url}`
+}
